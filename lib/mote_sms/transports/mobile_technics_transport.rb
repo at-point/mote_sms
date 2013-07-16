19d4554a53c2fbfcee145949aa2c2f506e656fe7
lukas <lukas.westermann@gmail.com>
2013-07-16 08:46:27 +0200

    release new version with key pinning

--- lib/mote_sms/transports/mobile_technics_transport.rb
+++ lib/mote_sms/transports/mobile_technics_transport.rb
@@ -29,12 +29,23 @@ module MoteSMS
     # Readable attributes
     attr_reader :endpoint, :username, :password, :options
 
+    # Internal: The default certificate store, adds all *CA.pem files
+    # from mote_sms/ssl_certs directory.
+    #
+    # Returns a OpenSSL::X509::Store
     def self.default_cert_store
       @cert_store ||= OpenSSL::X509::Store.new.tap do |store|
         Dir["#{CERTS_PATH}/*CA.pem"].each { |c| store.add_file c }
       end
     end
 
+    # Internal: Load a X509::Certificate based on the hostname, used to handle
+    # server certificate fingerprinting.
+    #
+    # host - The String with the hostname
+    #
+    # Returns OpenSSL::X509::Certificate or nil if no certificate for this host
+    #  is found
     def self.fingerprint_cert(host)
       cert = "#{CERTS_PATH}/#{host}.pem"
       OpenSSL::X509::Certificate.new(File.read(cert)) if File.exists?(cert)
--- lib/mote_sms/version.rb
+++ lib/mote_sms/version.rb
@@ -1,3 +1,3 @@
 module MoteSMS
-  VERSION = "1.0.2"
+  VERSION = "1.1.0"
 end
