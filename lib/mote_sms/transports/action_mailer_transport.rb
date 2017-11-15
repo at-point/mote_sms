57005d0677ed6f30167cbe7a1fcb51ebdfb3e599
Loris Gavillet <loris@at-point.ch>
2017-11-15 15:01:28 +0100

    Integrated

--- lib/mote_sms/transports/action_mailer_transport.rb
+++ lib/mote_sms/transports/action_mailer_transport.rb
@@ -3,9 +3,10 @@ require 'action_mailer'
 module MoteSMS
   # Internal: ActionMailer class to forward SMS to recipient.
   class ActionMailerSMSMailer < ::ActionMailer::Base
-    def forward_sms(recipient, sms, subject = nil)
+    def forward_sms(recipient, from, sms, subject = nil)
       subject ||= "SMS to #{sms.to.map(&:to_s).join(', ')}"
-      mail to: recipient, from: "#{sms.from} <#{recipient}>", subject: subject, body: sms.body
+      from ||= "#{sms.from} <#{recipient}>"
+      mail to: recipient, from: from, subject: subject, body: sms.body
     end
   end
 
@@ -24,11 +25,12 @@ module MoteSMS
   class ActionMailerTransport
     # Public: Read/change the recipient used when delivering the message.
     # Also accepts a Proc.
-    attr_accessor :recipient
+    attr_accessor :recipient, :from
 
     # Public: Create a new ActionMailerTransport instance
-    def initialize(recipient)
+    def initialize(recipient, from = nil)
       self.recipient = recipient
+      self.from = from
     end
 
     # Public: Sends message using ActionMailer to recipient.
@@ -40,7 +42,7 @@ module MoteSMS
     # Returns nothing.
     def deliver(message, options = {})
       to = recipient.respond_to?(:call) ? recipient.call(message) : recipient
-      ActionMailerSMSMailer.forward_sms(to, message, options[:subject].presence).deliver_now
+      ActionMailerSMSMailer.forward_sms(to, from, message, options[:subject].presence).deliver_now
 
       message.to
     end
